0af3a22ce107dde03cbfc4bd53936349225178d1
reddragonnm <neeleshojmishra@gmail.com>
2025-04-04 18:38:31 +0530

    Setup basic structure

--- client.py
+++ client.py
@@ -1,3 +1,47 @@
 import socket
 import threading
 import json
+import sys
+
+from chatui import init_windows, read_command, print_message, end_windows
+
+
+def usage():
+    print("usage: python client.py username address port")
+
+
+def print_messages():
+    pass
+
+
+def main(username, address, port):
+    init_windows()
+
+    s = socket.socket()
+    s.connect((address, port))
+
+    chat_thread = threading.Thread(target=print_messages)
+    chat_thread.start()
+
+    while True:
+        s = read_command()
+
+        if s == "/q":
+            break
+
+        print_message(f"Me: {s}")
+
+    chat_thread.join()
+    end_windows()
+
+
+if __name__ == "__main__":
+    try:
+        username = sys.argv[1]
+        address = sys.argv[2]
+        port = int(sys.argv[3])
+    except:
+        usage()
+        sys.exit(1)
+
+    main(username, address, port)
--- server.py
+++ server.py
@@ -1,3 +1,43 @@
 import socket
 import select
 import json
+import sys
+
+
+def usage():
+    print("usage: python server.py port")
+
+
+def main(port):
+    s = socket.socket()
+    s.bind(("localhost", port))
+    s.listen()
+
+    socks = {s}
+
+    while True:
+        available = select.select(socks, {}, {})[0]
+
+        for sock in available:
+            if sock is s:
+                new_sock, addr = s.accept()
+                socks.add(new_sock)
+
+                print(addr)
+            else:
+                data = sock.recv(4096)
+                print(data)
+
+                if len(data) == 0:
+                    socks.remove(sock)
+
+
+if __name__ == "__main__":
+    try:
+        port = int(sys.argv[1])
+        print(f"Server listening on port {port}")
+    except:
+        usage()
+        sys.exit(1)
+
+    main(port)
